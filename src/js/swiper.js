a25598f231f87b45a5b3ae3da599bad6159a14f8
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2024-12-23 19:45:17 +0200

    CSS-fix-general
    hero/gallery/header/footer/

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,16 +1,35 @@
-import Swiper from 'swiper';
-import '../node_modules/swiper/swiper-bundle.min.css';
-const swiper = new Swiper('.swiper-container', {
-  loop: true, // Enable looping
-  slidesPerView: 1, // One slide at a time
-  spaceBetween: 1, // Space between slides
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true, // Allow clicking on pagination bullets
-  },
-});
-export default swiper;
+// slickSlider.js
+import 'slick-carousel/slick/slick.css'; // Import Slick's CSS
+import 'slick-carousel/slick/slick-theme.css'; // Import Slick's theme CSS
+import 'slick-carousel'; // Import Slick's JavaScript
+import $ from 'jquery';
+
+// Function to initialize Slick
+export function initializeSlickSlider(selector) {
+  $(selector).slick({
+    infinite: true,
+    initialSlide: 0,
+    focusOnSelect: true,
+    centerMode: true,
+    centerPadding: '20px', // Reduce padding to avoid jumping
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: '20px', // Reduce padding on smaller screens
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          centerPadding: '20px', // Reduce padding on larger screens
+        },
+      },
+    ],
+    prevArrow: '<button class="slick-prev"></button>',
+    nextArrow: '<button class="slick-next"></button>',
+  });
+}
